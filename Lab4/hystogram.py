695a09689d6c71547960eed6ca8897bdbfde040b
Михаил Насонов <mnasonovy@gmail.com>
2023-12-23 11:38:58 +0400

    correcting mistakes

--- Lab4/hystogram.py
+++ Lab4/hystogram.py
@@ -4,26 +4,28 @@ import pandas as pd
 from collections import Counter
 from df_works import rating_filter, lemmatize_text
 
-
 logging.basicConfig(level=logging.INFO)
 
 
 def build_histogram(df: pd.DataFrame, rating: int) -> None:
-    """This function takes dataframe and show it's graph by class"""
+    """This function takes dataframe and shows its graph by class"""
     try:
         filtered_df = lemmatize_text(rating_filter(df, rating))
-        reviews = [f'{review} ' for review in filtered_df['Review text']]
-        splitted = str(reviews).split()
+        reviews = ' '.join(filtered_df['Review text']).split()
 
-        word_counts = Counter(splitted)
-        frequencies = word_counts.values()
-        names = word_counts.keys()
+        word_counts = Counter(reviews)
+        top_words = dict(word_counts.most_common(10))  # Выберите топ 10 самых частых слов
+
+        names = list(top_words.keys())
+        frequencies = list(top_words.values())
 
         plt.bar(names, frequencies)
         plt.xlabel('Word')
         plt.xticks(rotation=90)
         plt.ylabel('Word count')
         plt.title('Word use frequency by rating')
-        plt.show(block=True)  
+        plt.rcParams['font.sans-serif'] = 'Arial'
+        plt.rcParams['font.family'] = 'sans-serif'
+        plt.show(block=True)
     except Exception as exc:
         logging.error(f"Can not build histogram: {exc}\n{exc.args}\n")
